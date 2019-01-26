778e441f0c7f352ac0690e6260704a04e6b7d0e8
phenomenon95 <sharathmenon7500@gmail.com>
2019-01-26 13:13:16 -0800

    C Sharp Drill Page 111

---
+++ Page111/Page111/Methods.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page111
+{
+    public class Methods
+    {
+        //method to get the number squared
+        public static int Squared(int input)
+        {
+            input = input * input;
+            return input;
+        }
+
+        //method to get the remainder of 12
+        public static int Remain12(int input)
+        {
+            input %= 12;
+            return input;
+        }
+
+        //method to increment the first digit
+        public static int IncrementFirstDigit(int input)
+        {
+            if (input < 10 && input > -11)
+            {
+                input += 1;
+                return input;
+            }
+            else if (input < 100 && input > -110)
+            {
+                input += 10;
+                return input;
+            }
+            else if (input < 1000 && input > -1100)
+            {
+                input += 100;
+                return input;
+            }
+            else if (input < 10000 && input > -11000)
+            {
+                input += 1000;
+                return input;
+            }
+            else
+            {
+                input += 10000;
+                return input;
+            }
+        }
+    }
+}
---
+++ Page111/Page111/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Response;
+            do
+            {
+                Console.Write("\nPlease input a number:   ");
+                Response = Convert.ToInt32(Console.ReadLine());
+                if (Response > 40000 || Response < -40000)
+                    Console.WriteLine("You number is too large (must be within -40000 and 40000 to prevent overflow)");
+            } while (Response > 40000 || Response < -40000);
+            //setting some conditions to prevent problems
+
+            Console.WriteLine("\nYour number is:                               " + Response);
+            if (Response < 0)
+                Console.WriteLine("You number squared is:                        +" + Methods.Squared(Response));
+            else
+                Console.WriteLine("You number squared is:                        " + Methods.Squared(Response));
+            Console.WriteLine("You number divided by 12 has a remainder of:  " + Methods.Remain12(Response));
+            Console.WriteLine("You number incremented by its first digit is: " + Methods.IncrementFirstDigit(Response));
+
+            Console.ReadLine();
+
+        }
+    }
+}
