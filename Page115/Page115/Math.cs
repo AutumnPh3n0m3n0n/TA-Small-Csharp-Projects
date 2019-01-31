f4ab3cebd5f312e97024692c3f0a25bab42d95c4
phenomenon95 <sharathmenon7500@gmail.com>
2019-01-31 11:11:51 -0800

    C# Drills

---
+++ Page115/Page115/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page115
+{
+    class Math
+    {
+        public int oneNumber(int input)
+        {
+            if (input <= 500)
+            {
+                input *= 100;
+                return input;
+            }
+            else
+            {
+                input /= 10;
+                return input;
+            }
+        }
+
+        public int twoNumbers(int input1, int input2)
+        {
+            input1 *= input2;
+            return input1;
+        }
+        
+    }
+}
---
+++ Page115/Page115/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math toMix = new Math();
+            int response1;
+            char option;
+            int response2;
+            Console.Write("Please enter in a number: ");
+            response1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Do you want to enter in another number? (Press Y for Yes)  ");
+            option = Convert.ToChar(Console.ReadLine());
+            if (option == 'y' || option == 'Y')
+            {
+                Console.Write("\nPlease enter in another number: ");
+                response2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine(toMix.twoNumbers(response1, response2));
+            }
+            else
+            {
+                Console.WriteLine(toMix.oneNumber(response1));
+            }
+            Console.WriteLine("\n\nIf the second number is null and I passed it in as a function, an exception would happen");
+            Console.WriteLine("causing the program to crash.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
