9a6785290be05e7d7c178ff042170b8886aa1771
phenomenon95 <sharathmenon7500@gmail.com>
2019-01-31 11:34:01 -0800

    C# Drills

---
+++ Page117/Page117/Divide.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page117
+{
+    public static class Divide
+    {
+        private static bool ifEven = false;
+
+        //overloading the divide by two function
+        public static bool IfEven(int input)
+        {
+            if (input % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static void Div2(int input)
+        {
+            //check if the input is an odd number
+            if (ifEven == false)
+            {
+                float half = (float)input;
+                half /= 2;
+                Console.WriteLine(half);
+            }
+            else
+            {
+                input /= 2;
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
---
+++ Page117/Page117/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A static class cannot be instantiated
+            //Divide toDiv = new Divide();
+            Console.Write("Please enter in a number:   ");
+            int response = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("The number divided by 2 is   ");
+            Divide.Div2(response);
+            Console.ReadLine();
+        }
+
+    }
+}
